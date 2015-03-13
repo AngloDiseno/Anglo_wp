77621fe1a267d35da3d505416574049e23a9faee
Andre Ribet <aribet@gmail.com>
2015-03-12 23:15:09 -0300

    se mueve todo para tema de wp

---
+++ index_.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Anglo Diseño</title>
+</head>
+<body>
+	<header>
+		<div><img src="" alt="ANGLO"></div>
+		<h1><font face="wp_bogus_font">&nbsp;Anglo.cl</font></h1>
+	</header>
+<div>
+<ol>
+	<li>Trabajos
+		<ol>
+			<li>Desayunosconamor.cl</li>
+			<li>Jardinmargot.cl</li>
+			<li>Radio Taxi Regional</li>
+			<li>San Mart&iacute;n Hnos</li>
+		</ol>	
+	</li>
+	<li>Clientes
+		<ol>
+			<li>RTR</li>
+			<li>SMH</li>
+			<li>ML</li>
+			<li>AM</li>
+		</ol>	
+	</li>
+</ol>
+
+
+
+<p><a href="proyectos/anglo-wp">test wordpress</a></p>
+
+</div>
+	<footer>
+		pie
+	</footer>
+</body>
+</html>
+
